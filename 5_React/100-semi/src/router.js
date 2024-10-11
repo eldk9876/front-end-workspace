fa84fe890a07333ec1f72cbf5bb372d423ba6071
eldk9876 <dudalseldk@gmail.com>
2024-10-11 17:48:56 +0900

    React교육10일차 (2024-10-11)

--- 5_React/100-semi/src/router.js
+++ 5_React/100-semi/src/router.js
@@ -1,11 +1,11 @@
 import { createBrowserRouter } from "react-router-dom";
-import Layout from "./components";
+// import Layout from "./components";
 import Main from "./pages/Main";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Layout />,
+    element: "", //<Layout />,
     children: [{ index: true, element: <Main /> }],
   },
 ]);
